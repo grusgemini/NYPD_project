0711c56d3550114563d0cbe9d1c5e2b47a1efe80
Kacper Żurawski <kd.zurawski@gmail.com>
2025-06-15 12:25:47 +0200

    main

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
-import pandas as pd
 import argparse
 from mylib import fire_data_prep, alc_data_prep, pop_data_prep, area_data_prep, analysis
 
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('fires_data', type=str, help='Path to CSV file containing data about fire incidents in Poland')
+
 parser.add_argument('alc_data', type=str, help='Path to CSV file containing data about alcohol selling concessions Poland')
+parser.add_argument('area_data', type=str, help='Path to XLSX file containing data about area of administrative regions in Poland')
+parser.add_argument('fires_data', type=str, help='Path to CSV file containing data about fire incidents in Poland')
 parser.add_argument('pop_data', type=str, help='Path to XLS file containing data about population Poland')
-parser.add_argument('area_data', type=str, help='Path to XLSX file containing data about area Poland')
 
 args = parser.parse_args()
 
-fires_file = args.fires_data
-alcohol_file = args.alc_data
-pop_file = args.pop_data
-area_file = args.area_data
-
-fires = pd.read_csv(fires_file, skiprows=4)
-alcohol = pd.read_csv(alcohol_file, skiprows=4)
-pop = pd.read_excel(pop_file)
-area = pd.read_excel(area_file)
+alc_path = args.alc_data
+area_path = args.area_data
+fires_path = args.fires_data
+pop_path = args.pop_data
 
 
+alcohol = pd.read_csv(alc_path, skiprows=4)
+area = pd.read_excel(area_path)
+fires = pd.read_csv(fires_path, skiprows=4)
+pop = pd.read_excel(pop_path)
