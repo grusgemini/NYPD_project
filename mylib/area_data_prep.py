7931bf9c5699ba4095530c11ea3c0469f957213f
Kacper Żurawski <kd.zurawski@gmail.com>
2025-06-15 16:19:24 +0200

    commit

--- mylib/area_data_prep.py
+++ mylib/area_data_prep.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np 
 import openpyxl 
+import xlrd
 
 
 def setnames(df):
--- mylib/fire_data_prep.py
+++ mylib/fire_data_prep.py
@@ -1,10 +1,12 @@
 import pandas as pd
-import numpy as np 
-import openpyxl 
 
 
-def setnames(df):
-    df.columns = ['woj', 'powiat', 'gmina', 'total', 'ob. uzytecznosci publicznej', 'ob. mieszkalne', 'ob. produkcyjne', 'ob. magazynowe', 'transport', 'lasy']
+def load_file(file_path):
+    df = pd.read_csv(file_path)
+    return df
+
+def change_col_names(df):
+    df.columns = ['woj', 'powiat', 'gmina', 'total', 'ob. uzytecznosci publicznej', 'ob. mieszkalne', 'ob. produkcyjne', 'ob. magazynowe', 'transport', 'lasy', 'uprawy', 'inne', 'wybuchy']
     return df
 
 def drop_columns(df):
@@ -14,9 +16,16 @@ def drop_columns(df):
 def lower_case(df):
     for col in ['woj', 'powiat', 'gmina']:
         df[col] = df[col].str.lower()
-
     return df
 
+
+def final_fire_data(file_path):
+    df = load_file(file_path)
+    df_dropped = drop_columns(df) 
+    df_newnames = change_col_names(df_dropped)
+    final_df = lower_case(df_newnames)
+
+    return final_df
 
 
 
