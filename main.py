3f20358e0a1191c13a1329726cfd4ed6d7f5a9d5
Kacper Żurawski <kd.zurawski@gmail.com>
2025-06-27 23:38:51 +0200

    updates

--- main.py
+++ main.py
@@ -1,24 +1,71 @@
 import argparse
-from mylib import fire_data_prep, alc_data_prep, pop_data_prep, area_data_prep, analysis
+import os
+import pandas as pd
+from mylib import data_processing as dp
+from mylib import statistics as stat 
 
 
-parser = argparse.ArgumentParser()
+
+def main():
 
 
-parser.add_argument('alc_data', type=str, help='Path to CSV file containing data about alcohol selling concessions Poland')
-parser.add_argument('area_data', type=str, help='Path to XLSX file containing data about area of administrative regions in Poland')
-parser.add_argument('fires_data', type=str, help='Path to CSV file containing data about fire incidents in Poland')
-parser.add_argument('pop_data', type=str, help='Path to XLS file containing data about population Poland')
+    parser = argparse.ArgumentParser()
 
-args = parser.parse_args()
+    parser.add_argument('--input_directory', type=str, help='Path to directory containing files with data.')
+    parser.add_argument('--output_directory', type=str, help='Path to directory to save the results of analysis.')
 
-alc_path = args.alc_data
-area_path = args.area_data
-fires_path = args.fires_data
-pop_path = args.pop_data
+    args = parser.parse_args()
+
+    input_dir = args.input_directory
+    output_dir = args.output_directory
+    
+
+# READING THE FILES
+   
+    alcohol_data = dp.load_csv_file(os.path.join(input_dir, "alcohol_data.csv"))  #/Users/kacper/Desktop/NYPD_project/data/alcohol_data.csv
+    fire_incidents_data = dp.load_excel_file(os.path.join(input_dir, "fire_incidents.xlsx"))  #/Users/kacper/Desktop/NYPD_project/data/fire_incidents.xlsx
+    population_data = dp.load_excel_file(os.path.join(input_dir, "population_data.xls"))  #/Users/kacper/Desktop/NYPD_project/data/area_data.xlsx
+    area_data = dp.load_excel_file(os.path.join(input_dir, "area_data.xlsx"))  #/Users/kacper/Desktop/NYPD_project/data/population_data.xls 
 
 
-alcohol = pd.read_csv(alc_path, skiprows=4)
-area = pd.read_excel(area_path)
-fires = pd.read_csv(fires_path, skiprows=4)
-pop = pd.read_excel(pop_path)
+#PROCESSING THE ALCOHOL DATA
+
+    alcohol_cols_to_drop = ['Numer zezwolenia', 'Nazwa firmy', 'Kod pocztowy', 'Adres', 'Województwo', 'Data ważnosci']
+    alcohol_data = dp.drop_columns(alcohol_data, alcohol_cols_to_drop)
+    alcohol_data = dp.delete_spaces(alcohol_data, 'Miejscowość')
+    alcohol_data = dp.cut_after_comma(alcohol_data, 'Miejscowość')
+    alcohol_data = dp.count_unique(alcohol_data)
+
+#PROCESSING THE FIRE INCIDENTS DATA
+
+    fire_columns_to_drop = ['Województwo', 'Powiat', 'Ogółem zdarzeń rok 2023', 'Pożary rok 2023', 'Miejscowe zagrożenia rok 2023', 'Alarmy fałszywe rok 2023'] 
+    fire_incidents_data = dp.drop_columns(fire_incidents_data, fire_columns_to_drop)
+
+#PROCESSING THE POPULATION DATA
+
+    population_data = dp.load_excel_file(os.path.join(input_dir, "population_data.xls"), 8)  #/Users/kacper/Desktop/NYPD_project/data/area_data.xlsx
+    population_data = population_data.iloc[:,[1,2]]
+    population_data.columns = ['Kod', 'Populacja']
+
+
+#PROCESSING THE AREA DATA
+
+    area_columns_to_drop = ['Powierzchnia [ha]', 'Unnamed: 4', 'Unnamed: 5']
+    area_data = dp.drop_columns(area_data, area_columns_to_drop)
+    area_data = dp.delete_spaces(area_data, 'TERYT')
+    area_data = dp.delete_prefix(area_data, 'TERYT', '0')
+    area_data = dp.drop_rows(area_data, [1,2,3])
+
+#MERGIN THE DATA
+
+
+
+#CALCULATING STATISTICS
+
+
+#SAVING THE RESULTS 
+    print("Analysis complete. Results have been saved in:", output_dir)
+
+
+if __name__ == "__main__":
+    main()
---
+++ mylib/data_processing.py
@@ -0,0 +1,135 @@
+import pandas as pd
+
+def load_excel_file(filepath: str, rows_to_skip: int) -> pd.DataFrame:
+    """Loads an excel file as a Data Frame
+    :param file_path: Path to the CSV file that we want to load
+    :param rows_to_skio: How many rows you would like to drop from the top
+    """
+    return pd.read_excel(io=filepath, skiprows=rows_to_skip)
+
+
+def load_csv_file(filepath: str) -> pd.DataFrame:
+    """Loads a CSV file as a Data Frame
+    :param file_path: Path to the CSV file that we want to load"""
+    return pd.read_csv(filepath)
+
+
+def lower_the_case(data: pd.DataFrame, column: str) -> pd.DataFrame:
+    """Lowers the case of text in given column of a data frame. 
+    :param data: Input dataframe
+    :param column: Column that will be lowercased
+    """
+    if column not in data.columns:
+        raise ValueError(f"Column {column} not found in DataFrame.")
+    
+    data = data.copy()
+    data[column] = data[column].astype(str).str.lower()
+    return data
+
+
+def rename_column(data: pd.DataFrame, old_name: str, new_name: str) -> pd.DataFrame:
+    """Renames given column
+    :param data: Input data frame
+    :param old_name: Old column name
+    :param new_name: New column name
+    """
+    data = data.rename(columns={'old_name': 'new_name'})
+    return data
+
+
+def drop_columns(data: pd.DataFrame, columns: list) -> pd.DataFrame:
+    """Deletes given columns form a data frame.
+    :param data: Input dataframe
+    :param column: List of names of columns to drop"""
+    
+    data = data.copy()
+    data = data.drop(columns = columns)
+    return data
+
+
+def drop_rows(data: pd.DataFrame, rows: list) -> pd.DataFrame:
+    """Deletes given rows form a data frame.
+    :param data: Input dataframe
+    :param rows: List of numbers of rows to drop"""
+    data = data.copy()
+    rows = list(map(lambda x: x -1, rows))
+    data = data.drop(index = data.iloc[rows].index)
+    return data
+
+
+def delete_prefix(data: pd.DataFrame, column: str, prefixes: list) -> pd.DataFrame:
+    """Deletes a given prefix in a column of a dataframe.
+    :param data: Input DataFrame
+    :param column: Name of the column where the prefixes need to be removed
+    :param prefixes: List of strings with prefixes to be removed
+    """
+    if column not in data.columns:
+        raise ValueError(f"Column {column} not found in DataFrame.")
+    
+    data = data.copy()
+    data[column] = (
+        data[column]
+        .astype(str)
+        .apply(lambda x: next((x[len(p) :] for p in prefixes if x.startswith(p)), x))
+    )
+    return data
+
+
+def delete_spaces(data: pd.DataFrame, column: str) -> pd.DataFrame:
+    """Deletes spaces in a column of a dataframe.
+    :param data: Input DataFrame
+    :param column: Name of the column where the prefixes need to be removed
+    """ 
+    if column not in data.columns:
+        raise ValueError(f"Column {column} not found in DataFrame.")
+    
+    data = data.copy()
+    data[column] = data[column].str.replace(' ', '', regex=False) 
+    return data
+
+
+def cut_after_comma(data: pd.DataFrame, column: str) -> pd.DataFrame:
+    """Cuts the string after a comma
+    :param data: Input data frame
+    :param column: Column where we want to modify the strings
+    """
+    if column not in data.columns:
+        raise ValueError(f"Column {column} not found in DataFrame.")
+    
+    data = data.copy()
+    data[column] = data[column].str.replace(r',.*', '', regex=True)
+    return data
+
+
+def drop_last_char(data: pd.DataFrame, column: str) -> pd.DataFrame:
+    """Removes the last character from strings in a given column
+    :param data: Input dataframe
+    :param column: Column from which the last character is to be deleted
+    """
+    if column not in data.columns:
+        raise ValueError(f"Column {column} not found in DataFrame.")
+    
+    data = data.copy()
+    data[column] = data[column].astype(str).str[:-1]
+    return data
+
+
+def count_unique(data: pd.DataFrame, column: str) -> pd.DataFrame:
+    """Counts unique string values in a given column of a data frame. Returns a dataframe with two columns-    
+    the unique values and a number of occurences in the original data frame.
+    :param data: Input data frame
+    :param column: Column where we want to count the unique string values
+    """ 
+    if column not in data.columns:
+        raise ValueError(f"Column {column} not found in DataFrame.")
+    
+    counter = data[column].apply(lambda x: str(x).lower())
+    counter = counter.value_counts().reset_index()
+    counter.columns = [column, "Count"]
+    return counter
+
+
+
+
+
+
