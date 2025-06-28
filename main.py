fcd229d52533a303e7ea7e9c0a7140523fd25410
Kacper Żurawski <kd.zurawski@gmail.com>
2025-06-28 18:02:39 +0200

    update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import pandas as pd 
 from mylib import data_processing as dp
 
 
@@ -18,7 +19,6 @@ def main():
     
 
 # READING THE FILES
-   
     alcohol_data = dp.load_csv_file(os.path.join(input_dir, "alcohol_data.csv"))  #/Users/kacper/Desktop/NYPD_project/data/alcohol_data.csv
     fire_incidents_data = dp.load_excel_file(os.path.join(input_dir, "fire_incidents.xlsx"), 0)   #/Users/kacper/Desktop/NYPD_project/data/fire_incidents.xlsx
     population_data = dp.load_excel_file(os.path.join(input_dir, "population_data.xls"), 8)  #/Users/kacper/Desktop/NYPD_project/data/area_data.xlsx
@@ -26,7 +26,6 @@ def main():
 
 
 #PROCESSING THE ALCOHOL DATA
-
     alcohol_cols_to_drop = ['Numer zezwolenia', 'Nazwa firmy', 'Kod pocztowy', 'Adres', 'Województwo', 'Data ważności']
     alcohol_data = dp.drop_columns(alcohol_data, alcohol_cols_to_drop)
     alcohol_data = dp.delete_spaces(alcohol_data, 'Miejscowość')
@@ -34,8 +33,6 @@ def main():
     alcohol_data = dp.count_unique(alcohol_data, 'Miejscowość')
 
 #PROCESSING THE FIRE INCIDENTS DATA
-
-    
     fire_columns_to_drop = ['Województwo', 'Powiat', 'Ogółem zdarzeń rok 2023', 'Pożary rok 2023', 'Miejscowe zagrożenia rok 2023', 'Alarmy fałszywe rok 2023'] 
     fire_incidents_data = dp.drop_columns(fire_incidents_data, fire_columns_to_drop)
     fire_incidents_data = dp.lower_the_case(fire_incidents_data, 'Gmina')
@@ -43,7 +40,6 @@ def main():
 
 
 #PROCESSING THE POPULATION DATA
-
     population_data = population_data.iloc[:,[0,1,2]]
     population_data.columns = ['Nazwa jednostki','Kod', 'Populacja']
     population_data = dp.delete_prefix(population_data, 'Kod', '0')
@@ -56,7 +52,6 @@ def main():
 
 
 #PROCESSING THE AREA DATA
-
     area_columns_to_drop = ['Powierzchnia [ha]', 'Unnamed: 4', 'Unnamed: 5']
     area_data = dp.drop_columns(area_data, area_columns_to_drop)
     area_data = dp.lower_the_case(area_data, 'Nazwa jednostki')
@@ -70,39 +65,55 @@ def main():
 #ALCOHOL VS POPULATION
     alcohol_vs_population = dp.custom_merge(alcohol_data, population_data, 'Miejscowość', 'Nazwa jednostki')
     corr_alc_vs_pop = alcohol_vs_population["Count"].corr(alcohol_vs_population["Populacja"])
-    dp.save_results("Correlation between population and number of alcohol selling companies", corr_alc_vs_pop, os.path.join(output_dir, "Corr_alcohol_vs_population.txt"))
 
 #FIRE INCIDENTS VS ALCOHOL 
     fire_vs_alcohol = dp.custom_merge(fire_incidents_data, alcohol_data, 'Gmina', 'Miejscowość')
     corr_all_fire_vs_alc = fire_vs_alcohol["Count"].corr(fire_vs_alcohol["Ogółem zdarzeń rok 2024"])
     corr_actual_fire_vs_alc = fire_vs_alcohol["Count"].corr(fire_vs_alcohol["Pożary rok 2024"])
     corr_false_fire_vs_alc = fire_vs_alcohol["Count"].corr(fire_vs_alcohol["Alarmy fałszywe rok 2024"])
-    dp.save_results("Correlation between number of all fire incidents and number of alcohol selling companies", corr_all_fire_vs_alc, os.path.join(output_dir, "Corr_all_fire_vs_alcohol.txt"))
-    dp.save_results("Correlation between number of actual fire incidents and number of alcohol selling companies", corr_actual_fire_vs_alc, os.path.join(output_dir, "Corr_actual_fire_vs_alcohol.txt"))
-    dp.save_results("Correlation between number of false reports of fire incidents and number of alcohol selling companies", corr_false_fire_vs_alc, os.path.join(output_dir, "Corr_fake_fire_vs_alcohol.txt"))
 
 #FIRE INCIDENTS VS POPULATION
     fire_vs_population = dp.custom_merge(fire_incidents_data, population_data, 'TERYT', 'Kod')
     corr_all_fire_vs_pop = fire_vs_population["Populacja"].corr(fire_vs_population["Ogółem zdarzeń rok 2024"])
     corr_actual_fire_vs_pop = fire_vs_population["Populacja"].corr(fire_vs_population["Pożary rok 2024"])
     corr_false_fire_vs_pop = fire_vs_population["Populacja"].corr(fire_vs_population["Alarmy fałszywe rok 2024"])
-    dp.save_results("Correlation between number of all fire incidents and population", corr_all_fire_vs_pop, os.path.join(output_dir, "Corr_all_fire_vs_population.txt"))
-    dp.save_results("Correlation between number of actual fire incidents and population", corr_actual_fire_vs_pop, os.path.join(output_dir, "Corr_actual_fire_vs_population.txt"))
-    dp.save_results("Correlation between number of false reports of fire incidents and population", corr_false_fire_vs_pop, os.path.join(output_dir, "Corr_fake_fire_vs_population.txt"))
 
 #FIRE INCIDENTS VS AREA
     fire_vs_area = dp.custom_merge(fire_incidents_data, area_data, 'TERYT', 'TERYT')
     corr_all_fire_vs_area = fire_vs_area["Powierzchnia [km2]"].corr(fire_vs_area["Ogółem zdarzeń rok 2024"])
     corr_actual_fire_vs_area = fire_vs_area["Powierzchnia [km2]"].corr(fire_vs_area["Pożary rok 2024"])
     corr_false_fire_vs_area = fire_vs_area["Powierzchnia [km2]"].corr(fire_vs_area["Alarmy fałszywe rok 2024"])
-    dp.save_results("Correlation between number of all fire incidents and area", corr_all_fire_vs_area, os.path.join(output_dir, "Corr_all_fire_vs_area.txt"))
-    dp.save_results("Correlation between number of actual fire incidents and area", corr_actual_fire_vs_area, os.path.join(output_dir, "Corr_actual_fire_vs_area.txt"))
-    dp.save_results("Correlation between number of false reports of fire incidents and area", corr_false_fire_vs_area, os.path.join(output_dir, "Corr_fake_fire_vs_area.txt"))
-
-
 
 
 #SAVING THE RESULTS 
+
+    results = pd.DataFrame({
+        'correlation':['Correlation between population and number of alcohol selling companies: ', 
+                       'Correlation between number of all fire incidents and number of alcohol selling companies: ',
+                       'Correlation between number of actual fire incidents and number of alcohol selling companies: ',
+                       'Correlation between number of false reports of fire incidents and number of alcohol selling companies: ',
+                       'Correlation between number of all fire incidents and population: ',
+                       'Correlation between number of actual fire incidents and population: ',
+                       'Correlation between number of false reports of fire incidents and population: ',
+                       'Correlation between number of all fire incidents and area: ',
+                       'Correlation between number of actual fire incidents and area: ',
+                       'Correlation between number of false reports of fire incidents and area: '],
+
+        'value': [corr_alc_vs_pop, 
+                  corr_all_fire_vs_alc,
+                  corr_actual_fire_vs_alc,
+                  corr_false_fire_vs_alc,
+                  corr_all_fire_vs_pop,
+                  corr_actual_fire_vs_pop,
+                  corr_false_fire_vs_pop, 
+                  corr_all_fire_vs_area,
+                  corr_actual_fire_vs_area,
+                  corr_false_fire_vs_area]
+    })
+
+    results.to_csv(os.path.join(output_dir, "Results.txt"), sep='\t', index=False, header = False)
+
+
     print("Analysis complete. Results have been saved in:", output_dir)
 
 
