2cb5297f3029b6ecf7ab70f983ea86e1dfb2cebd
Kacper Żurawski <kd.zurawski@gmail.com>
2025-06-29 21:19:21 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='NYPD Final Project',
+    version='1.0', 
+    author='Kacper Żurawski', 
+    author_email='kd.zurawski@student.uw.edu.pl',
+    long_description=open('README.md').read(),
+    url='https://github.com/grusgemini/NYPD_project', 
+    packages=find_packages(),
+    python_requires='>=3.7',
+    
+)
